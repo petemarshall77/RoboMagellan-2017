a524bd817087c3aa942a7b0aaf5c4e1e125520ac
Pete Marshall <petemarshall77@gmail.com>
2017-03-27 16:09:52 -0700

    Initial commit: drive test

---
+++ python/drive_test.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+# Robomagellan 2016
+#
+
+# Import all the things
+import time
+from logger import Logger
+from powersteering import *
+from compass import *
+from gps import *
+from camera import *
+from robot import *
+
+#Port Definitions
+steeringPower_port_name = '/dev/ttyACM0'
+compass_port_name = '/dev/ttyACM1'
+GPS_port_name = '/dev/ttyUSB0'
+
+def june16course():
+  drive_to_cone(1600, 33.77845666666666, 118.41903666666667)
+  drive_to_cone(1600, 33.77858166666667, 118.41884166666667)
+
+def july7course():
+  drive_to_cone(1600, 33.77854, 118.418916667)
+  drive_to_cone(1600, 33.7786883333333335, 118.41892)
+
+def july14course():
+  drive_to_cone(1600, 33.77849666666667, 118.41903166666667)
+  drive_to_cone(1600, 33.77864833333334, 118.41890666666667)
+
+def aug11course():
+  drive_to_cone(1600, 33.77851666666667, 118.41900833333334)
+  drive_to_cone(1600, 33.77867166666667, 118.41894333333333)
+
+def nov8course():
+  drive_to_cone(1600, 33.77864666666667, 118.41893166666667)
+  drive_to_cone(1600, 33.778468333333336, 118.41905)
+
+def dec6course(robot):
+  robot.drive_to_cone(1600, 33.77868, 118.41895166666667)
+  robot.drive_to_cone(1600, 33.778513333333336, 118.41902)
+
+def dec20course(robot):
+  robot.drive_to_waypoint(1600, 33.77862833333333, 118.41897)
+  robot.drive_to_waypoint(1600, 33.778395, 118.41895666666667)
+  robot.drive_to_waypoint(1600, 33.77862833333333, 118.41897)
+
+def jan10course(robot):
+  robot.drive_to_cone(2, 33.778621666666666, 118.41890833333333)
+  robot.drive_to_cone(2, 33.778481666666664, 118.41897333333333)
+
+def jan24course(robot):
+  robot.drive_to_cone(2, 33.779722, 118.41888888)
+  
+def feb16course(robot):
+  robot.drive_to_cone(2, 33.77863833333333, 118.41891333333334)
+  robot.drive_to_cone(2, 33.778286666666666, 118.41896166666666)
+  robot.drive_to_cone(2, 33.77840833333333, 118.41915166666666)
+  robot.drive_to_cone(2, 33.77863833333333, 118.41891333333334)
+
+def mar6course(robot):
+  robot.drive_to_cone(2, 33.77861, 118.41888333333333)
+  robot.drive_to_cone(2, 33.778515, 118.41898666666667)
+
+def mar13course(robot):
+  robot.drive_to_cone(1.5, 33.77858833333333, 118.41896833333334)
+  robot.drive_to_cone(1.5, 33.77849333333333, 118.41899833333333)
+  
+def mar30course(robot):
+  robot.drive_to_cone(1.5, 33.77863333333333, 118.41894166666667)
+  robot.drive_to_waypoint(1.5, 33.778418333333335, 118.41889)
+  robot.drive_to_cone(1.5, 33.778531666666666, 118.419085)
+  robot.drive_to_cone(1.5, 33.77863333333333, 118.41894166666667)
+  robot.drive_to_waypoint(1.5, 33.778571666666664, 118.41876)
+
+def apr3course(robot):
+  robot.drive_to_cone(1.5, 33.77863833333333, 118.41891833333334, 1580)
+  robot.drive_to_cone(1.5, 33.778345, 118.41901833333333)
+  robot.drive_to_cone(1.5, 33.77863833333333, 118.41891833333334)
+  robot.drive_to_waypoint(1.5, 33.77857, 118.41875833333333)
+
+def dec12course(robot):
+  robot.drive_to_cone(1.5, 33.77813166666667, 118.41887333333334)
+  robot.drive_to_cone(1.5, 33.77825833333333, 118.41907666666667)
+#========================================================
+#Main program starts here
+#========================================================
+def main():
+    # Start logging
+    datalog = Logger()
+
+    # Start the power/steering serial port (causes the Arduido to reset so
+    # wait a few seconds for that to happen)
+    power_steering = PowerSteering(steeringPower_port_name, 9600, datalog)
+    time.sleep(5)
+
+    # Start the compass serial port
+    compass = Compass(compass_port_name, 9600, datalog)
+
+    # Start the GPS serial port
+    gps = GPS(GPS_port_name, 4800, datalog)
+
+    # Set the Arduino to Drive Mode by sending control string
+    power_steering.set_mode_drive()
+    time.sleep(5)
+
+    compass_thread = CompassThread(compass, datalog)
+    compass_thread.start()
+
+    gps_thread = GPSThread(gps, datalog)
+    gps_thread.start()
+
+    camera = Camera(datalog)
+    camera_thread = CameraThread(camera, datalog)
+    camera_thread.start()
+
+    robot = Robot(power_steering, compass, gps, camera, datalog)
+
+    try:
+        gps.get_GPS()
+        datalog.write("Armed and Ready - Press Button")
+        while compass.get_bump_switch_state() == False:
+            pass
+        datalog.write("Go!!!")
+        dec12course(robot)
+
+    except KeyboardInterrupt:
+        pass
+
+    #========================================================
+    #Main program stops here
+    #========================================================
+    robot.stop_driving()
+    power_steering.set_mode_stop()
+
+    compass_thread.join()
+    gps_thread.join()
+    camera_thread.join()
+    time.sleep(5)
+    datalog.write('Done!!!')
+    del datalog
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/powersteering.py
@@ -0,0 +1,61 @@
+from serial import Serial
+
+class PowerSteering:
+
+    def __init__(self, port_name, baud_rate, logger):
+        self.logger = logger
+        self.logger.write("Starting Power/Steering Communications")
+        self.serial = Serial(port_name, baud_rate)
+        self.current_power = 1500
+        self.current_speed = 0
+
+    def set_mode_drive(self):
+        self.logger.write("Set to DRIVE mode")
+        self.serial.write("+++DRIVE+++")
+        self.serial.write("\n")
+
+    def set_mode_stop(self):
+        self.logger.write("Set to STOPPED mode")
+        self.serial.write("+++STOP+++")
+        self.serial.write("\n")
+        self.serial.flush()
+
+    def set_pwr_and_steer(self, steer_value, target_speed, new_speed, kickStart = 0):
+      # Process Steering Values
+      if steer_value > 500:
+        steer_value = 500
+      elif steer_value < -500:
+        steer_value = -500
+      steer_value = 1500-steer_value
+      
+      #KickStart
+      if kickStart > 1500:
+        self.current_power = kickStart
+        
+      # Calculate the power value
+      delta_speed = target_speed - new_speed
+      acceleration = new_speed - self.current_speed
+      delta_power = delta_speed * 0.1 - acceleration * 0.0
+      self.current_power = self.current_power + delta_power
+      self.logger.write("Power steer- target: %.2f, current: %.2f, power: %d" % (target_speed, new_speed, self.current_power))
+      self.current_speed = new_speed
+      if self.current_power > 1650:
+          self.current_power = 1500
+
+      self.serial.write(str(steer_value) + "," + str(int(self.current_power)))
+      self.serial.write('\n')
+      self.serial.flush()
+
+    def reverse(self, steer_value, power_value):
+        if steer_value > 500:
+            steer_value = 500
+        elif steer_value < -500:
+            steer_value = -500
+
+        steer_value = 1500-steer_value
+
+        self.logger.write("Reversing: steer=%d, power=%d" % (steer_value, power_value))
+
+        self.serial.write(str(steer_value) + "," + str(power_value))
+        self.serial.write('\n')
+        self.serial.flush()
