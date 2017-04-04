e699211fabb4fd65833000c8a1c0a3559eb05b22
Pete Marshall <petemarshall77@gmail.com>
2017-04-03 17:50:11 -0700

    Add /dev/ttyUSB to usb probe

--- python/usb_probe.py
+++ python/usb_probe.py
@@ -1,30 +1,34 @@
 # Probe for RoboMagellan USB devices
 import subprocess, re
 
-installed_devices = {'95437313334351516052': 'chias-test',
-                     '95437313335351514292': 'speedometer-test',
-		     '9543731313535140B152': 'chias',
-		     '95437313334351519012': 'compass-bump',
-		     '9543731333435151B2E1': 'speedometer',
-		     '9543731333535131D0D2': 'killswitch'}
+installed_devices = {
+    '95437313334351516052': 'chias-test',
+    '95437313335351514292': 'speedometer-test',
+    'A6008iFZ':             'atmega-328-test', 
+    '9543731313535140B152': 'chias',
+    '95437313334351519012': 'compass-bump',
+    '9543731333435151B2E1': 'speedometer',
+    '9543731333535131D0D2': 'killswitch'
+    }
+port_families = ['/dev/ttyACM', '/dev/ttyUSB']
 
 def probe():
     ports = {}
-    for index in range(10):
-        port = '/dev/ttyACM%s' % index
-        command = 'udevadm info -a -n %s' % port 
-        try:
-            out = subprocess.check_output(command.split(),
-                                          stderr=subprocess.STDOUT)
-            
-            regex = re.compile("{serial}==\"(\w+)\"")
-            match = regex.search(out)
-            if match:
-                serial_num = match.group(1)
-                if serial_num in installed_devices:
-                    ports[installed_devices[serial_num]] = port
-        except:
-            pass
+    for port_family in port_families:
+        for index in range(10):
+            port = '%s%s' % (port_family, index)
+            command = 'udevadm info -a -n %s' % port
+            try:
+                out = subprocess.check_output(command.split(),
+                                            stderr=subprocess.STDOUT)
+
+                regex = re.compile("{serial}==\"(\w+)\"")
+                match = regex.search(out)
+                if match:
+                    serial_num = match.group(1)
+                    if serial_num in installed_devices:
+                        ports[installed_devices[serial_num]] = port
+            except:
+                pass
 
     return ports
-
